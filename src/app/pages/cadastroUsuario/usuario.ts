0aded5d6525b7a015b601a568eadde83ee6ed4e9
Jean Carlos Lefchak <jean_2103@hotmail.com>
2018-11-20 15:25:57 -0200

    alteracoes!

--- src/app/pages/cadastroUsuario/usuario.ts
+++ src/app/pages/cadastroUsuario/usuario.ts
@@ -6,51 +6,18 @@ import { Usuario } from '../../models/usuario';
     selector: 'usuario',
     templateUrl: './usuario.html',
     styleUrls: ['usuario.scss'],
-    providers: [Usuario]
+    providers: [UsuarioService] // aqui vai os services que voce vai usar, se usar mais de um separa por virgula
 })
 export class UsuarioPage implements OnInit {
+    public usuario: Usuario = new Usuario();// instancia do usuario
 
-    public salvar(usuario: Usuario) {
-        this.usuarioService.saveUser(usuario).subscribe((res: Usuario) => {
-            this.buscando = false;
-        }, err => {
-            this.buscando = false;
+    constructor(public usuarioService: UsuarioService) {// aqui voce precisa criar uma variave do usuarioService pra poder usar nos metodos
+
+    }
+
+    public salvar() {
+        this.usuarioService.save(this.usuario).subscribe((res: Usuario) => { // esse save é um metodo abstrato todas as classes tem elez
+            this.usuario = res; // res é o retorno do servidor, o que esta voltando do servidor depois de salvo
         })
-    }
-
-    public buscarListaModelos() {
-        this.buscando = true;
-        this.modeloService.buscarModelos().subscribe((res: Modelo[]) => {
-            for (let x of res) {
-                if (x.imagem != undefined) {
-                    x.imagem = JSON.parse(x.imagem);
-                }
-                console.log(x);
-                this.listaModelo.push(x);
-            }
-            this.buscando = false;
-            this.imagemSelecionada(this.listaModelo[0]);
-        }, err => {
-            this.buscando = false;
-        })
-    }
-
-    public excluir(m: Modelo, index) {
-        let modelo = { ...m };
-        if (index == undefined) {
-            index = this.verificaIndex(modelo);
-        }
-        modelo.deletado = true;
-        modelo.imagem = JSON.stringify(modelo.imagem);
-        this.salvar(modelo);
-        this.listaModelo.splice(index, 1);
-    }
-
-    public verificaIndex(modelo: Modelo) {
-        for (let i = 0;i < this.listaModelo.length;i++) {
-            if (modelo.imagem == this.listaModelo[i].imagem) {
-                return i;
-            }
-        }
     }
 }
